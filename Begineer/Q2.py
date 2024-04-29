c974eb5a5acd9d1dc21bc49cf57cdcc41e136fc2
Kuldeepsinh Vaghela <kvaghela@stevens.edu>
2024-04-29 13:36:37 -0400

    Change q2

--- Begineer/Q2.py
+++ Begineer/Q2.py
@@ -1,10 +1,13 @@
 inp_string = input("Please enter the string\n")
-alphabets = 0
-numbers = 0
-for x in inp_string:
-    if x.isalpha():
-        alphabets += 1
-    elif x.isnumeric():
-        numbers += 1
-    
-print("Alphabets:",alphabets, "&","Number:",numbers)
+def counter(inp_string):
+    alphabets = 0
+    numbers = 0
+    for x in inp_string:
+        if x.isalpha():
+            alphabets += 1
+        elif x.isnumeric():
+            numbers += 1
+        
+    print("Alphabets:",alphabets, "&","Number:",numbers)
+
+counter(inp_string)
